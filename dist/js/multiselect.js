60215e1ee2c814ba633a32174e4f55f648e579f3
Serhan Apaydın <zerhan@gmail.com>
2020-03-12 13:06:53 +0300

    Add new option: "panelCls"

--- dist/js/multiselect.js
+++ dist/js/multiselect.js
@@ -77,7 +77,7 @@ if (typeof jQuery === 'undefined') {
                 $leftSelect = $emptySelect.clone().attr('id', id + '_from'),
                 $rightSelect = $emptySelect.clone().attr('id', id + '_to'),
                 $panel = $(
-                    '<div class="panel panel-default panel-anim MultiSelect">' +
+                    '<div class="panel panel-default panel-anim MultiSelect ' + (settings.panelCls || '') + '">' +
                         '<div class="panel-heading fw-b">' + labelHtml + closingDivHtml +
                         '<div class="panel-body">' +
                             rowHtml +
